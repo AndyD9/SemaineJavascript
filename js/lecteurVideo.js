ab1ff914d577ec0efbde2b2f7d3333012c50a575
AndyD9 <32742219+AndyD9@users.noreply.github.com>
2017-12-18 22:00:03 +0100

    Update Content

--- js/lecteurVideo.js
+++ js/lecteurVideo.js
@@ -9,6 +9,8 @@ var btn = document.querySelector('.play-pause-btn');
 var btnmultifonction = document.querySelector('.play-pause-btn');
 var element = document.querySelector('.video');
 var btnmute = document.querySelector('.mutebtn');
+var textcurtime = document.querySelector('.curtimetexte');
+var textdurtime = document.querySelector('.durtimetexte')
 
 
 document.addEventListener('DOMContentLoaded',function(){
@@ -43,9 +45,22 @@ function stopvideo() {
 }
 
 function updateBar (){
-  var pourcentage = Math.floor((100/ mediaPlayer.duration)* mediaPlayer.currentTime);
+  var pourcentage = ((100/ mediaPlayer.duration)* mediaPlayer.currentTime);
   progressBar.value = pourcentage;
+  var curmins = Math.floor(video.currentTime / 60);
+  var cursecs = Math.floor(video.currentTime - curmins * 60);
+  var durmins = Math.floor(video.duration / 60);
+  var dursecs = Math.round(video.duration - durmins * 60);
+  if (cursecs < 10){
+      cursecs = "0"+cursecs;
+  }
+  if(dursecs < 10){
+    dursecs = "0"+dursecs;
+  }
+  textcurtime.innerHTML = curmins+":"+cursecs;
+  textdurtime.innerHTML = durmins+":"+dursecs;
 }
+
 
 function seek(e) {
   var percent = e.offsetX / this.offsetWidth;
